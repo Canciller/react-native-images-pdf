dd4b43fc4a487193e410e74eace10d83a6055d04
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2023-03-05 02:45:06 -0600

    style: fix lint errors

--- example/src/App.js
+++ example/src/App.js
@@ -4,39 +4,44 @@ import { launchImageLibrary } from 'react-native-image-picker';
 import { pickDirectory } from 'react-native-document-picker';
 import { Button, StyleSheet, View } from 'react-native';
 export default function App() {
-    const selectImages = async () => {
-        try {
-            const result = await launchImageLibrary({
-                mediaType: 'photo',
-                selectionLimit: 0,
-            });
-            if (result.assets) {
-                const assets = result.assets;
-                const imagePaths = assets.map((asset) => asset.uri);
-                const resultPickDir = await pickDirectory();
-                if (!resultPickDir) {
-                    return;
-                }
-                const outputDirectory = resultPickDir.uri;
-                const outputFilename = 'example.pdf';
-                await createPdf({
-                    imagePaths,
-                    outputDirectory,
-                    outputFilename,
-                });
-            }
+  const selectImages = async () => {
+    try {
+      const result = await launchImageLibrary({
+        mediaType: 'photo',
+        selectionLimit: 0,
+      });
+      if (result.assets) {
+        const assets = result.assets;
+        const imagePaths = assets.map((asset) => asset.uri);
+        const resultPickDir = await pickDirectory();
+        if (!resultPickDir) {
+          return;
         }
-        catch (e) {
-            console.error(e);
-        }
-    };
-    return (React.createElement(View, { style: styles.root },
-        React.createElement(Button, { title: "Select images", onPress: selectImages })));
+        const outputDirectory = resultPickDir.uri;
+        const outputFilename = 'example.pdf';
+        await createPdf({
+          imagePaths,
+          outputDirectory,
+          outputFilename,
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return React.createElement(
+    View,
+    { style: styles.root },
+    React.createElement(Button, {
+      title: 'Select images',
+      onPress: selectImages,
+    })
+  );
 }
 const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { NativeModules, Platform } from 'react-native';
-const LINKING_ERROR = `The package 'react-native-images-pdf' doesn't seem to be linked. Make sure: \n\n` +
-    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-    '- You rebuilt the app after installing the package\n' +
-    '- You are not using Expo Go\n';
+const LINKING_ERROR =
+  `The package 'react-native-images-pdf' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
 const ImagesPdf = NativeModules.ImagesPdf
-    ? NativeModules.ImagesPdf
-    : new Proxy({}, {
+  ? NativeModules.ImagesPdf
+  : new Proxy(
+      {},
+      {
         get() {
-            throw new Error(LINKING_ERROR);
+          throw new Error(LINKING_ERROR);
         },
-    });
+      }
+    );
 export function createPdf(options) {
-    return ImagesPdf.createPdf(options);
+  return ImagesPdf.createPdf(options);
 }
 export function getDocumentsDirectory() {
-    return ImagesPdf.getDocumentsDirectory();
+  return ImagesPdf.getDocumentsDirectory();
 }
